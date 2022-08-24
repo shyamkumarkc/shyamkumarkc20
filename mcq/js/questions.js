f895c05620cd29ac2d54b4cc504cc93512a782f1
shyamkumarkc <kcshyamkumar20@gmail.com>
2022-08-24 20:53:16 +0545

    Add files via upload

---
+++ mcq/js/questions.js
@@ -0,0 +1,255 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does C-ASBA stands for?",
+    answer: "Centralized Application for ASBA based issuance",
+    options: [
+	      "Centralized Application for ASBA based issuance",
+      "Controlled Application Supported by Bulk Account",
+      "Centralized Accounts Supported by Blocked Account",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is a Beneficial Owner (BO) regarding DEMAT account?",
+    answer: "DEMAT account holder",
+    options: [
+      "Bank who opens the DEMAT account",
+      "Capital who owns the DEMAT account of customer",
+      "DEMAT account holder",
+      "Both 1 & 2 are correct"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What are the sources of dirty/black money?",
+    answer: "All of the above",
+    options: [
+		"Human trafficking, Extortion",
+      "Smuggling, Tax Evasion",
+           "None of the above",
+		   
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Consortium financing is defined as:",
+    answer: "Financing a project by multiple banks",
+    options: [
+      "Financing a project by a single bank",
+      "Financing a project by multiple banks",
+      "None of the above",
+		   
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Fixed deposits are:",
+    answer: "Repayable after Maturity",
+    options: [
+	
+      "Repayable on demand",
+      "Repayable after Maturity",
+      "Non repayable",
+      "Negotiable"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Cheque payment/ transfer is not honored if:",
+   answer: "All of the above",
+ options: [
+      "Pen ink differs",
+     "Writing differs",
+     "Cheque is issued 6 months before",
+     "All of the above"
+   ]
+ },
+ {
+    numb: 7,
+    question: "A minor account is converted into the major account if the account holder attains:",
+   answer: "18 Years",
+ options: [ 
+
+
+      "16 Years",
+     "18 Years",
+     "17 Years",
+     "15 Years"
+   ]
+ },
+ {
+    numb: 8,
+    question: "What is IPS?",
+   answer: "Inter Bank Payment System",
+ options: [
+
+      "Inter Branch Payment System",
+     "Inter Office Payment System",
+     "Inter Bank Payment System",
+     "Inter Bank Process System"
+   ]
+ },
+ {
+    numb: 9,
+    question: "A cheque issued by Bank is valid for",
+   answer: "6 months from the issue date",
+ options: [
+      "1 month from the issue date",
+     "15 days from the issue date",
+     "3 months from the issue date",
+     "6 months from the issue date"
+   ]
+ },
+ {
+    numb: 10,
+    question: "Which of the following is not directly controlled by NRB?",
+   answer: "Base Rate",
+ options: [
+
+      "CRR",
+     "CAR",
+     "Base Rate",
+     "Bank Rate"
+   ]
+ },
+ {
+    numb: 11,
+    question: "A cheque book can be issued in following accounts:",
+   answer: "Saving account",
+ options: [
+
+      "Saving account",
+     "Fixed deposit account",
+     "Call account",
+     "All of the above"
+   ]
+ },
+ {
+    numb: 12,
+    question: "A party who issued the cheque is called as:",
+   answer: "Drawer",
+ options: [
+
+      "Payee",
+     "Drawee",
+     "Drawer",
+     "None of the above"
+   ]
+ },
+ {
+    numb: 13,
+    question: "Thumb impression of the customer is mandatory to open the account of:",
+   answer: "All of the above",
+ options: [
+      "Illiterate person",
+     "Literate person",
+     "None of the above",
+     "All of the above"
+   ]
+ },
+ {
+    numb: 14,
+    question: "A shell entity",
+   answer: "Both A and B",
+ options: [
+      "Has no physical presence in the country in which it is incorporated",
+     "Has no fixed assets or does not have active business operation",
+	 "Is an unregistered entity",
+	 "Both A and B"
+  
+   ]
+ },
+ {
+    numb: 15,
+    question: "What is the full form of FIU in Nepal?",
+   answer: "Financial Information Unit",
+ options: [
+
+
+      "Financial Intelligence Unit",
+     "Financial Information Unit",
+     "Full Investigation Unit",
+     "Further Investigation Unit"
+   ]
+ },
+ {
+    numb: 16,
+    question: "A PEP who no longer holds a prominent public position from last … years is not a PEP:",
+   answer: "5 years",
+ options: [
+      "1 year",
+     "2 years",
+     "5 years",
+     "7 years"
+   ]
+ },
+  {
+    numb: 17,
+    question: "Tax rate applicable on interest payment to individual is:",
+   answer: "5.00%",
+ options: [
+
+
+      "10.00%",
+     "5.00%",
+     "25.00%",
+     "15.00%"
+   ]
+ },
+  {
+    numb: 18,
+    question: "Current Account is terminated into dormant status if the account is not operated for:",
+   answer: "6 months",
+ options: [
+
+      "12 months",
+     "36 months",
+     "9 months",
+     "6 months"
+   ]
+ },
+  {
+    numb: 19,
+   question: "Saving Account is terminated into dormant status if the account is not operated for:",
+   answer: "36 months",
+ options: [
+
+      "12 months",
+     "36 months",
+     "9 months",
+     "6 months"
+   ]
+ },
+  {
+    numb: 20,
+    question: "Account review frequency for High Risk Account is:",
+   answer: "1 year",
+ options: [
+
+      "1 year",
+     "2 years",
+     "3 years",
+     "4 years"
+   ]
+ },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+ 
+];
