704220bb00b6687e67efd027494f9c1a382c7dc4
shyamkumarkc <kcshyamkumar20@gmail.com>
2025-12-20 12:26:05 +0545

    Create service.js

---
+++ ielts/service.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'portfolio-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js',
+  'https://shyamkumarkc20.com.np/js/meroshare.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
